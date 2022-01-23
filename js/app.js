58744b55b86e2e593ea2fe14da757c7f4459a948
AwsAlyasjen <awsalyasjen@gmail.com>
2022-01-23 17:53:17 +0200

    addThreeQuestions

--- js/app.js
+++ js/app.js
@@ -23,3 +23,66 @@ else if (skipWelcomingMessage==false && yourgender.toUpperCase()=="FEMALE"|| you
 }else{
     ;
 }
+//for task 6
+// reversedDays.push(days[i]);
+
+/*function addElements(array){
+   
+    answers.push(addElements);
+    return answers;
+  
+}*/
+
+let answers = [];
+for(let i=0; i<3;i++){
+    if(i==0){
+        var checkAddress = prompt("Do you live in Jordan? (note that all the answers should be (Yes/No):)");
+        if(checkAddress.toUpperCase()==("YES")){
+           
+            answers.push(checkAddress);
+        }
+         else if(checkAddress.toUpperCase()==("NO")){
+           
+            answers.push(checkAddress);
+        }
+        else {
+            answers.push("valid");
+        }
+        
+        
+        }
+        if(i==1){
+            var checkSpecialty = prompt("Are you an IT graduate?? (note that all the answers should be (Yes/No):)");
+            if(checkSpecialty.toUpperCase()==("YES")){
+           
+                answers.push(checkSpecialty);
+            }
+             else if(checkSpecialty.toUpperCase()==("NO")){
+               
+                answers.push(checkSpecialty);
+            }
+            else {
+                answers.push("valid");
+            }
+        
+        }
+        if(i==2){
+                var checkUser = prompt("Did you receive your username and password?? (note that all the answers should be (Yes/No):)");
+                if(checkUser.toUpperCase()==("YES")){
+           
+                    answers.push(checkUser);
+                }
+                 else if(checkUser.toUpperCase()==("NO")){
+                   
+                    answers.push(checkUser);
+                }
+                else {
+                    answers.push("valid");
+                }
+        }
+
+       
+    }
+    console.log(answers);
+   
+    
